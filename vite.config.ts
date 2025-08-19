01afc914cca4478eced7225ab1e0b1e5047bec21
ANGEOM21 <idrisanjounghan313@gmail.com>
2025-08-20 04:24:43 +0700

    main

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
-      base: "/my-portfolio/",
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
